2957e7e46d5f8e8c5306226fa4a597b062f90f4d
Matthew Morley <matt@mpcm.com>
2012-04-05 08:10:24 -0300

    Format cleanup.

--- server.php
+++ server.php
@@ -3,26 +3,27 @@ header('Content-type: application/json; charset=utf-8');
 
 // load the shared functions into the global scope
 // these happen to be the functions related to specification examples
+// but normally you would load your custom functions here
 require_once('spec_functions.php');
 
-// load the base class
+// load the base jsonrpc2 processing class
 require_once('jsonrpc2.class.php');
 
-// load the transport processing layer, and the base handling class
+// load the transport processing layer, extends base class
 require_once('transport_http.class.php');
 
-// create a instance of the transport layer
+// create a instance of the transport layer, with method maps
 $server = new transport_http(
-	array(
-		'subtract' 		=> 'subtract',
-		'sum' 			=> 'sum',
-		'notify_hello' 	=> 'notify_hello',
-		'notify_sum' 	=> 'notify_sum',
-		'get_data' 		=> 'get_data'
-	)
+  array(
+    'subtract'  => 'subtract',
+    'sum'  => 'sum',
+    'notify_hello' => 'notify_hello',
+    'notify_sum' => 'notify_sum',
+    'get_data' => 'get_data'
+  )
 );
 
-// override the keys to check, if so desired
-//$server->keys[] = 'msg';
+// add additional query string keys to check, if so desired
+$server->keys[] = 'msg';
 
 print $server->process();
